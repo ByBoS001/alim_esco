bf30973a968dc1c035aef78f8c3200b5fc479817
Bruno Ortega <brunoxavierortega@gmail.com>
2026-08-15 18:44:42 -0500

    Backend alimentacion_escolar

--- db/index.js
+++ db/index.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 // Creamos un Pool de conexiones con pg
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Importamos nuestros esquemas desde models
